0a4ae8c99127a5eee95a05889a4083fd682a1dc7
Emmanuel Kala <emkala@gmail.com>
2014-01-09 17:17:00 +0300

    Remove cache and log directories
    Do not version the cache and log directories. These directories should
    only be created during installation. See the README file for more info.

--- application/logs/2013-12-03.log.php
+++
@@ -1,3 +0,0 @@
-<?php defined('SYSPATH') or die('No direct script access.'); ?>
-
-2013-12-03 11:53:56 +00:00 --- error: core.uncaught_exception
--- application/logs/2013-12-12.log.php
+++
@@ -1,4 +0,0 @@
-<?php defined('SYSPATH') or die('No direct script access.'); ?>
-
-2013-12-12 18:18:47 +00:00 --- error: core.uncaught_exception
-2013-12-12 18:18:47 +00:00 --- error: core.uncaught_exception
